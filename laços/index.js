0d651385ce21c498a124b29ab3730ffe9192d1db
Roger_Júnior <144962638+roger2507@users.noreply.github.com>
2023-09-13 21:26:10 -0300

    Add files via upload

---
+++ laços/index.js
@@ -0,0 +1,68 @@
+/*const numero1 = 1
+const numero2 = 2
+if (numero1 === numero2){
+    console.log("Os numeros sao iguais")
+} else{
+    console.log("Os numeros nao sao iguais")
+}
+*/
+/*
+
+
+
+
+
+let numero1 = 3
+let numero2 = 4
+if (numero1 > numero2){
+console.log("E maior")
+}else if (numero1 < numero2){
+console.log("E menor")
+}else{
+    console.log("Sao iguais")
+}
+*/
+
+/*
+let pokemonInicial = "squirtle(agua)"
+
+if(pokemonInicial === "bulbassaur(planta veneno)"){
+console.log(pokemonInicial)
+
+}else if(pokemonInicial === "charmander(fogo)"){
+    console.log(pokemonInicial)
+
+}else{
+console.log(pokemonInicial)
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+let i = o
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
